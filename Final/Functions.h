d5bf2b0f543cd056261f8df3c0ad085841117e6e
Олександр Затогін <zatogin1995@gmail.com>
2019-06-09 23:12:05 +0300

    Main logic

---
+++ Final/Functions.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+/* ^^ these are the include guards */
+
+/* Prototypes for the functions */
+
+#endif
--- Final/pch.h
+++ Final/pch.h
@@ -11,4 +11,5 @@
 
 // TODO: add headers that you want to pre-compile here
 
+
 #endif //PCH_H
